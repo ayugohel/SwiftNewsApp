ea2ced8d776f938b52bef9e26ba333cfd1a567cd
Aayushi <gohelayushi@gmail.com>
2020-07-25 17:04:20 -0400

    Initial Commit

---
+++ SwiftNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftNewsApp
+//
+//  Created by Ayushi on 2020-07-25.
+//  Copyright © 2020 Ayushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
